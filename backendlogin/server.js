2c7832e90ea590cf34903916c5658f568d7bf4de
David Meza <jose.mezaf@uanl.edu.mx>
2026-03-03 22:52:22 -0600

    Forzando redeploy

--- backendlogin/server.js
+++ backendlogin/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+console.log("VERSION NUEVA RESTAURANTES");
 
 const app = express();
 
@@ -9,7 +10,7 @@ app.use(express.json());
 
 // Rutas
 app.use('/api/usuarios', require('./routes/usuarios.routes'));
-app.use('/api/restaurante', require('./routes/restaurantes.routes')); 
+app.use('/api/restaurantes', require('./routes/restaurantes.routes')); 
 //app.use('/api/mesas', require('./routes/mesas.routes'));
 //app.use('/api/pedidos', require('./routes/pedidos.routes'));
 
